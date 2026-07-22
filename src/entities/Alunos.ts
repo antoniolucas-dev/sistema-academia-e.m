a25ecd9d670a4639475275b746f16fa1bad3133f
MATHEUS FELIPE PAULA DE ALMEIDA <a95693845@rn.senac.local>
2026-07-22 14:22:57 -0300

    implementação de código em arquivos

---
+++ src/entities/Alunos.ts
@@ -0,0 +1,11 @@
+export default class Aluno{
+
+    constructor(
+        public id:string,
+        public nome:string,
+        public idade:number,
+        public modalidade:string,
+        public telefone:string
+    ){}
+
+}
