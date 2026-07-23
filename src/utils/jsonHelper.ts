0b0716b78a17011712df32c1ef59fa398d196eb5
MATHEUS FELIPE PAULA DE ALMEIDA <a95693845@rn.senac.local>
2026-07-23 11:56:08 -0300

    corrige nome do arquivo alunoRoutes

--- src/utils/jsonHelper.ts
+++ src/utils/jsonHelper.ts
@@ -1,18 +1,39 @@
 import fs from "fs";
 
-export function lerArquivo(caminho: string) {
-    if (!fs.existsSync(caminho)) {
+
+export function lerArquivo(caminho:string){
+
+    if(!fs.existsSync(caminho)){
         return [];
     }
 
-    const dados = fs.readFileSync(caminho, "utf-8");
+
+    const dados = fs.readFileSync(
+        caminho,
+        "utf-8"
+    );
+
 
     return JSON.parse(dados);
+
 }
 
-export function salvarArquivo(caminho: string, dados: any) {
+
+
+export function salvarArquivo(
+    caminho:string,
+    dados:any
+){
+
     fs.writeFileSync(
         caminho,
-        JSON.stringify(dados, null, 2)
+        JSON.stringify(
+            dados,
+            null,
+            2
+        )
     );
+
 }
+
+
