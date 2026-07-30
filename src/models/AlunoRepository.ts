cda219180452217cbd3e0cab446177ceec1397cf
ERICK FELIPE RODRIGUES DE MOURA <a95691393@rn.senac.local>
2026-07-30 12:52:03 -0300

    correção de erro e implementação de rotas

--- src/models/AlunoRepository.ts
+++ src/models/AlunoRepository.ts
@@ -1,22 +1,28 @@
-import {Aluno} from "../entities/Alunos";
+import {Aluno} from "../entities/Aluno";
 import {lerArquivo,salvarArquivo} from "../utils/jsonHelper";
 import {gerarId} from "../utils/idGenerator";
 
 
 const ARQUIVO="dados/alunos.json";
+
+
 export class AlunoRepository{
 
 
 listar(){
+
     return lerArquivo(ARQUIVO);
 
 }
+
+
 
 criar(
     nome:string,
     idade:number,
     modalidade:string
 ){
+
 
 const aluno = new Aluno(
     gerarId(),
@@ -25,36 +31,57 @@ const aluno = new Aluno(
     modalidade
 );
 
+
 const alunos=this.listar();
+
+
 alunos.push(aluno);
+
 
 salvarArquivo(
     ARQUIVO,
     alunos
 );
 
+
 return aluno;
+
+
 }
 
+
+
 buscar(id:string){
+
 return this.listar()
 .find(
 (aluno:any)=>aluno.id===id
 );
+
+
 }
 
+
+
 remover(id:string){
+
+
 const alunos=this.listar();
+
 
 const novos=alunos.filter(
 (a:any)=>a.id!==id
 );
+
 
 salvarArquivo(
 ARQUIVO,
 novos
 );
 
+
 }
+
+
 
 }
