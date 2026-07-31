9f7a9e822d05e286eabcb5338c99730ef28454b3
ERICK FELIPE RODRIGUES DE MOURA <a95691393@rn.senac.local>
2026-07-31 11:09:15 -0300

    atualiza aluno.ts

--- src/entities/Aluno.ts
+++ src/entities/Aluno.ts
@@ -1,5 +1,4 @@
 export class Aluno {
-<<<<<<< HEAD
     id: string;
     nome: string;
     idade: number;
@@ -17,17 +16,3 @@ export class Aluno {
         this.modalidade = modalidade;
     }
 }
-=======
-    public id: string;
-    public nome: string;
-    public idade: number;
-    public modalidade: string;
-  
-    constructor(id: string, nome: string, idade: number, modalidade: string) {
-      this.id = id;
-      this.nome = nome;
-      this.idade = idade;
-      this.modalidade = modalidade;
-    }
-  }
->>>>>>> cda219180452217cbd3e0cab446177ceec1397cf
